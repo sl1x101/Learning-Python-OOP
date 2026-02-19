0edbf963639e599f0c59d84acf27a86f76a9e8a5
slx1 <nontawat012345@hotmail.com>
2026-02-19 12:02:29 +0900

    build oop pt

---
+++ mian.py
@@ -0,0 +1,20 @@
+#object orientated programming in python
+
+#create a class 
+
+class Adventurer:
+    def __init__(self,name,level): 
+        self.name = name
+        self.level = level
+    def close_range(self):
+        print(f"name: {self.name} level: {self.level}")
+    def long_range(self):
+        print(f"name:{self.name} level: {self.level}")
+    def set_level (self,level):
+        self.level = level
+sword_man = Adventurer("Bod",20)
+sword_man.set_level(50)
+sword_man.close_range()
+
+
+        
