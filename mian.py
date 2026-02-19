8e1669b136c016e0632c0f984d5fd30b8fb8b1b7
slx1 <nontawat012345@hotmail.com>
2026-02-19 15:40:12 +0900

    basic_oop

--- mian.py
+++ mian.py
@@ -10,10 +10,10 @@ class Adventurer:
         print(f"name: {self.name} level: {self.level}")
     def long_range(self):
         print(f"name:{self.name} level: {self.level}")
-    def set_level (self,level):
+    def set_level (self,level): #modify method
         self.level = level
-sword_man = Adventurer("Bod",20)
-sword_man.set_level(50)
+sword_man = Adventurer("Bod",20) 
+sword_man.set_level(50)  
 sword_man.close_range()
 
 
